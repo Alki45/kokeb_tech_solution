f7ebe2c83fee622d3db26e9f813005e533f6f69f
alki45 <alikibretmuhamed@gmail.com>
2025-06-02 15:41:02 +0300

    Git deployemnt configuration

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
-
-
-const isProd = process.env.NODE_ENV === 'production';
-
+// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
   output: 'export',
-  basePath: isProd ? '/kokeb_tech_solution' : '',
-  assetPrefix: isProd ? '/kokeb_tech_solution/' : '',
-  trailingSlash: true,
+  basePath: '/kokeb_tech_solution',
+  assetPrefix: '/kokeb_tech_solution/',
 };
 
 module.exports = nextConfig;
